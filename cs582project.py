cc592814cd6c02e57c2e81e1ad12f145fa2f4abc
AkhilBollu <akhilbollu04@gmail.com>
2023-05-05 21:31:05 -0500

    Instead of passing sys.argv to execute_from_command_line(), you can simply call the method with no arguments

--- cs582project.py
+++ cs582project.py
@@ -76,17 +76,14 @@ def my_fbv(request, *args, **kwargs):
 import os
 import sys
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trydjango.settings")
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+    except ImportError:
+        raise ImportError("Couldn't import Django. Did you forget to activate a virtual environment?") 
+    execute_from_command_line()
+
     
 
 # Application definition
