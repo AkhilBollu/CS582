dc3ab8ad1118af555663638bedeed86ea095dd07
AkhilBollu <akhilbollu04@gmail.com>
2023-05-06 00:35:47 -0500

    updated models

--- src/vehicles/models.py
+++ src/vehicles/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.urls import reverse
 # Create your models here.
-class Product(models.Model):
-    title       = models.CharField(max_length=120) # max_length = required
-    description = models.TextField(blank=True, null=True)
+class Vehicles(models.Model):
+    make      = models.CharField(max_length=120) # max_length = required
+    model = models.TextField(blank=True, null=True)
     price       = models.DecimalField(decimal_places=2, max_digits=10000)
-    summary     = models.TextField(blank=False, null=False)
+    info     = models.TextField(blank=False, null=False)
     featured    = models.BooleanField(default=False) # null=True, default=True
 
     def get_absolute_url(self):
-        return reverse("products:product-detail", kwargs={"id": self.id}) #f"/products/{self.id}/"
+        return reverse("vehicles:vehicle-detail", kwargs={"id": self.id}) #f"/vehicles/{self.id}/"
