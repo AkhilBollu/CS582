c819efa8341ad72efb2fc73f4e0c28e5c8271d80
AkhilBollu <akhilbollu04@gmail.com>
2023-05-06 00:46:26 -0500

    modified views

--- src/vehicles/views.py
+++ src/vehicles/views.py
@@ -1,51 +1,51 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .forms import ProductForm
-from .models import Product
+from .forms import VehicleForm
+from .models import Vehicles
 
 
-def product_create_view(request):
-    form = ProductForm(request.POST or None)
+def vehicle_create_view(request):
+    form = VehicleForm(request.POST or None)
     if form.is_valid():
         form.save()
-        form = ProductForm()
+        form = VehicleForm()
     context = {
         'form': form
     }
-    return render(request, "products/product_create.html", context)
+    return render(request, "vehicles/vehicle_create.html", context)
 
 
-def product_update_view(request, id=id):
-    obj = get_object_or_404(Product, id=id)
-    form = ProductForm(request.POST or None, instance=obj)
+def vehicle_update_view(request, id=id):
+    obj = get_object_or_404(Vehicle, id=id)
+    form = VehicleForm(request.POST or None, instance=obj)
     if form.is_valid():
         form.save()
     context = {
         'form': form
     }
-    return render(request, "products/product_create.html", context)
+    return render(request, "vehicles/vehicle_create.html", context)
 
 
-def product_list_view(request):
-    queryset = Product.objects.all() # list of objects
+def vehicle_list_view(request):
+    queryset = Vehicle.objects.all() # list of objects
     context = {
         "object_list": queryset
     }
-    return render(request, "products/product_list.html", context)
+    return render(request, "vehicles/vehicle_list.html", context)
 
-def product_detail_view(request, id):
-    obj = get_object_or_404(Product, id=id)
+def vehicle_detail_view(request, id):
+    obj = get_object_or_404(Vehicle, id=id)
     context = {
         "object": obj
     }
-    return render(request, "products/product_detail.html", context)
+    return render(request, "vehicles/vehicle_detail.html", context)
 
 
-def product_delete_view(request, id):
-    obj = get_object_or_404(Product, id=id)
+def vehicle_delete_view(request, id):
+    obj = get_object_or_404(Vehicle, id=id)
     if request.method == "POST":
         obj.delete()
         return redirect('../../')
     context = {
         "object": obj
     }
-    return render(request, "products/product_delete.html", context)
+    return render(request, "vehicles/vehicle_delete.html", context)
