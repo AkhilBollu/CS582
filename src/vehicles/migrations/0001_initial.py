f31168cfc91bcd1e22d64065b8f17cd88f398cfe
AkhilBollu <akhilbollu04@gmail.com>
2023-05-06 00:01:53 -0500

    created vehicle model

--- src/vehicles/migrations/0001_initial.py
+++ src/vehicles/migrations/0001_initial.py
@@ -12,13 +12,13 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Product',
+            name='Vehicles',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=120)),
-                ('description', models.TextField(blank=True, null=True)),
+                ('make', models.CharField(max_length=120)),
+                ('model', models.TextField(blank=True, null=True)),
                 ('price', models.DecimalField(decimal_places=2, max_digits=10000)),
-                ('summary', models.TextField()),
+                ('details', models.TextField()),
             ],
         ),
     ]
